653aef667d4ed7ee6da2088babc5dbf80b808c64
vishnugajmal07 <gajamalvishnu02@gmail.com>
2025-02-19 23:10:04 +0530

    Day 11 | #75DaysOfCode - Committed today's progress..!
    Learned a littel about iframe and done a one basic Script
    on that.

--- src/Assignments/Dream11.java
+++ src/Assignments/Dream11.java
@@ -13,8 +13,7 @@ public class Dream11
 				
 				driver.get("https://www.dream11.com/");
 				
-				
-				Thread.sleep(4000);
+				Thread.sleep(2000);
 				
 				driver.switchTo().frame(0); // Switch to the first iframe
 				driver.findElement(By.xpath("//div[@class='txtField']/input")).sendKeys("1234567");
