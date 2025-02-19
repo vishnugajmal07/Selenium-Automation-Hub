b60ef0b96071d171f50e6354354d852b06348dfd
vishnugajmal07 <gajamalvishnu02@gmail.com>
2025-02-19 22:42:34 +0530

    Day 10 | #75DaysOfCode - Committed today's progress..!

---
+++ src/Assignments/Dream11.java
@@ -0,0 +1,24 @@
+package Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dream11
+{
+		public static void main(String[] args) throws InterruptedException
+		{
+				ChromeDriver driver=new ChromeDriver();
+				
+				driver.manage().window().maximize();
+				
+				driver.get("https://www.dream11.com/");
+				
+				
+				Thread.sleep(4000);
+				
+				driver.switchTo().frame(0); // Switch to the first iframe
+				driver.findElement(By.xpath("//div[@class='txtField']/input")).sendKeys("1234567");
+				driver.switchTo().defaultContent();	
+				
+		}
+}
